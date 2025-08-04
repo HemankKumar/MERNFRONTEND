22765f8ecbc059aa8e22890000e8d649a0aa5851
HemankKumar <hemankkumar92@gmail.com>
2025-08-04 22:24:26 +0530

    Your commit message

--- src/config/url.js
+++ src/config/url.js
@@ -1,3 +1,3 @@
-export const server_url="http://localhost:2010";
+export const server_url="mongodb+srv://hemankkumar92:<FayTTWPB0QNEgPQS>@hemank.pwynqys.mongodb.net/?retryWrites=true&w=majority&appName=Hemank";
 
 // export const server_url="https://mern-stack-backend-62ek.onrender.com";
